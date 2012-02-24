8851be57bdf2768759207ed8c9ec2439a7702a1a
Mauricio Reyes <mauricio.reyes@summumnet.com>
2012-02-24 19:23:13 -0430

    Improve readability

--- background.js
+++ background.js
@@ -1,11 +1,34 @@
 var tabId = undefined;
 var running = false;
 
+var def_get_source = "function get_source(){ \
+            url=window.url; \
+            domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:''); \
+            $.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}}); \
+            css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g); \
+            $.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)}); \
+            js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g); \
+            $.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)}); \
+            $.each(css_src,function(a,b){ \
+                $.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})}); \
+            $.each(js_src,function(a,b){ \
+                $.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})}); \
+            return source \
+        }";
+
+var code = def_get_source +
+            "var origin=get_source(); \
+            try{clearInterval(interval)}catch(e){} \
+            interval=setInterval(function(){ \
+                source=get_source(); \
+                if(origin!=source){origin=source;history.go(0)} \
+            },2000)";
+
 chrome.tabs.onUpdated.addListener(function() {
     if (running){
         chrome.tabs.executeScript(tabId, { file: "jquery-1.7.1.min.js" }, function() {
             chrome.tabs.executeScript(tabId,
-                {code:"function get_source(){url=window.url;domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:'');$.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}});css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g);$.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)});js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g);$.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)});$.each(css_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});$.each(js_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});return source}var origin=get_source();try{clearInterval(interval)}catch(e){}interval=setInterval(function(){source=get_source();if(origin!=source){origin=source;history.go(0)}},2000)"});
+                {code:code});
         });
     }
     else{
@@ -30,72 +53,8 @@ function actionClicked(tab) {
         chrome.browserAction.setIcon({path:"stop.png"});
         chrome.tabs.executeScript(tabId, { file: "jquery-1.7.1.min.js" }, function() {
             chrome.tabs.executeScript(tabId,
-                {code:"function get_source(){url=window.url;domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:'');$.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}});css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g);$.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)});js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g);$.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)});$.each(css_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});$.each(js_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});return source}var origin=get_source();interval=setInterval(function(){source=get_source();if(origin!=source){origin=source;history.go(0)}},2000)"});
+                {code:code});
         });
         running = true;
     }
 }
-
-/*
-function get_source(){
-    url = window.url;
-    domain = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
-    // get html data
-    $.ajax({
-        url: url,
-        async:false,
-        cache:false,
-        dataType:"text",
-        success: function(data) {
-            source = data;
-        }
-    });
-    // get css uri's
-    css_src = source.match(/(href=(\"|\'))[^http].+(\.css)/g);
-    $.each(css_src, function(i,v){
-        css_src[i]=css_src[i].replace(/href=(\"|\')/,domain);
-    });
-    // get js uri's
-    js_src = source.match(/(src=(\"|\'))[^http].+(\.js)/g);
-    $.each(js_src, function(i,v){
-        js_src[i]=js_src[i].replace(/src=(\"|\')/,domain);
-    });
-    // get css data
-    $.each(css_src, function(i,uri){
-        $.ajax({
-            url: uri,
-            async:false,
-            cache:false,
-            dataType:"text",
-            success: function(data) {
-                source += data;
-            }
-        });
-    });
-    // get js data
-    $.each(js_src, function(i,uri){
-        $.ajax({
-            url: uri,
-            async:false,
-            cache:false,
-            dataType:"text",
-            success: function(data) {
-                source += data;
-            }
-        });
-    });
-    
-    return source;
-}
-
-var origin = get_source()
-interval = setInterval(function(){
-    source = get_source();
-    
-    if (origin != source){
-        origin = source;
-        history.go(0);
-    }
-},1000);
-// compress with http://jscompress.com/
-*/
