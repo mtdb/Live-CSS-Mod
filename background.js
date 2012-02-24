ce8b0f5de6485fcf2ab02fcb502ce2cafab0ac40
Mauricio Reyes <mauricio@appjango.com>
2012-02-23 23:16:31 -0430

    Fix tabId bug

--- background.js
+++ background.js
@@ -1,29 +1,40 @@
-var interval;
-var origin = "";
-var running = true;
+var tabId = undefined;
+var running = false;
 
 chrome.tabs.onUpdated.addListener(function() {
-    play();
+    if (running){
+        chrome.tabs.executeScript(tabId, { file: "jquery-1.7.1.min.js" }, function() {
+            chrome.tabs.executeScript(tabId,
+                {code:"function get_source(){url=window.url;domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:'');$.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}});css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g);$.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)});js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g);$.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)});$.each(css_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});$.each(js_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});return source}var origin=get_source();try{clearInterval(interval)}catch(e){}interval=setInterval(function(){source=get_source();if(origin!=source){origin=source;history.go(0)}},2000)"});
+        });
+    }
+    else{
+        chrome.browserAction.setIcon({path:"play.png"});
+    }
 });
 
-function play() {
-    chrome.tabs.executeScript(null, { file: "jquery-1.7.1.min.js" }, function() {
-        if (running){
-            chrome.browserAction.setIcon({path:"play.png"});
-            chrome.tabs.executeScript(null, {code:"try{clearInterval(interval)}catch(e){}"});
-            running = false;
-        }
-        else{
-            chrome.browserAction.setIcon({path:"stop.png"});
-            chrome.tabs.executeScript(null,
-                {code:"function get_source(){url=window.url;domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:'');$.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}});css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g);$.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)});js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g);$.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)});$.each(css_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});$.each(js_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});return source}var origin=get_source();interval=setInterval(function(){source=get_source();if(origin!=source){origin=source;history.go(0)}},1e3)"});
-            running = true;
-        }
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.windows.getCurrent(function(win) {
+        chrome.tabs.getSelected(win.id, actionClicked);
     });
-}
+});
 
-chrome.browserAction.onClicked.addListener(play);
-play();
+function actionClicked(tab) {
+    if (running){
+        chrome.browserAction.setIcon({path:"play.png"});
+        chrome.tabs.executeScript(tabId, {code:"try{clearInterval(interval)}catch(e){}"});
+        running = false;
+    }
+    else{
+        tabId = tab.id;
+        chrome.browserAction.setIcon({path:"stop.png"});
+        chrome.tabs.executeScript(tabId, { file: "jquery-1.7.1.min.js" }, function() {
+            chrome.tabs.executeScript(tabId,
+                {code:"function get_source(){url=window.url;domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:'');$.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}});css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g);$.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)});js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g);$.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)});$.each(css_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});$.each(js_src,function(a,b){$.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});return source}var origin=get_source();interval=setInterval(function(){source=get_source();if(origin!=source){origin=source;history.go(0)}},2000)"});
+        });
+        running = true;
+    }
+}
 
 /*
 function get_source(){
