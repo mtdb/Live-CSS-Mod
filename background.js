d7b64e09ce0b82971f260e3e39793a180d76bead
Mauricio Reyes <mauricio@appjango.com>
2012-03-07 22:31:49 -0430

    fix bug when not loaded the css file in the document change history.go(0) for window.location.reload()

--- background.js
+++ background.js
@@ -2,17 +2,22 @@ var tabId = undefined;
 var running = false;
 
 var def_get_source = "function get_source(){ \
-            url=window.url; \
-            domain=location.protocol+'//'+location.hostname+(location.port?':'+location.port:''); \
+            url=location.href; \
+            if (location.protocol == 'file:') \
+                domain=location.href.replace(/\\/[^\\/]+\\.html/,''); \
+            else \
+                domain=location.protocol+'\/\/'+location.hostname+(location.port?':'+location.port:''); \
             $.ajax({url:url,async:false,cache:false,dataType:'text',success:function(a){source=a}}); \
             css_src=source.match(/(href=(\"|\'))[^http].+(\.css)/g); \
-            $.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)}); \
+            if (css_src!=null){ \
+                $.each(css_src,function(a,b){css_src[a]=css_src[a].replace(/href=(\"|\')/,domain)}); \
+                $.each(css_src,function(a,b){ \
+                    $.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});} \
             js_src=source.match(/(src=(\"|\'))[^http].+(\.js)/g); \
-            $.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)}); \
-            $.each(css_src,function(a,b){ \
-                $.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})}); \
-            $.each(js_src,function(a,b){ \
-                $.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})}); \
+            if (js_src!=null){ \
+                $.each(js_src,function(a,b){js_src[a]=js_src[a].replace(/src=(\"|\')/,domain)}); \
+                $.each(js_src,function(a,b){ \
+                    $.ajax({url:b,async:false,cache:false,dataType:'text',success:function(a){source+=a}})});} \
             return source \
         }";
 
@@ -21,7 +26,7 @@ var code = def_get_source +
             try{clearInterval(interval)}catch(e){} \
             interval=setInterval(function(){ \
                 source=get_source(); \
-                if(origin!=source){origin=source;history.go(0)} \
+                if(origin!=source){origin=source;window.location.reload()} \
             },2000)";
 
 chrome.tabs.onUpdated.addListener(function() {
